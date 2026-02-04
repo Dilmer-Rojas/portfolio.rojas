72bc4a0ef52296f2f0ea1a21903c736004c789b0
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2026-02-03 21:49:35 -0500

    Reportes de Adidas

---
+++ src/data/reports.ts
@@ -0,0 +1,13 @@
+export const REPORTS = {
+    adidas: {
+        title: "Reporte Ejecutivo Adidas",
+        image: "/reports/reporte-adidas-final.png",
+        pdf: "/reports/Reporte_Adidas_CEO.pdf"
+    },
+
+    amazon: {
+        title: "Reporte Ejecutivo Amazon",
+        image: "/reports/reporte-amazon.png",
+        pdf: "/reports/reporte-amazon.pdf"
+    }
+};
